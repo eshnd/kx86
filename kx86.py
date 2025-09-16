a3563a16bf02b0a507924048d1ed2f7f4b1d70cf
eshnd <eshaan2031@icloud.com>
2025-09-15 23:11:10 -0500

    arguments algorithm

--- kx86.py
+++ kx86.py
@@ -15,6 +15,13 @@ def draw_pixel(x, y, color):
     add edi, ecx
     mov dword [edi], 0x{color[1:]}"""
 
+def arguments(cmd, splitter, amount):
+    replacement = random.randint(1, 10000)
+    while str(replacement) in cmd:
+        replacement = random.randint(1, 10000)
+    cmd = cmd.replace(splitter, str(replacement), amount)
+    return cmd.split(str(replacement))
+
 def kx86_compile(body, splitter = ";"):
     final = ""
     labels = []
@@ -24,41 +31,40 @@ def kx86_compile(body, splitter = ";"):
         cmd = body[i].strip()
         if cmd.strip() == "":
             continue
-        cmd = [cmd[:cmd.index(":")].strip(), cmd[cmd.index(":") + 1:].strip()]
+        cmd = arguments(cmd, ":", 1)
         match cmd[0].strip():
             case "pixel":
-                cmd[1] = [cmd[1][:cmd[1].index(",")].strip(), cmd[1][cmd[1].index(",") + 1:].strip()]
-                cmd[1] = [cmd[1][0], cmd[1][1][:cmd[1][1].index(",")].strip(), cmd[1][1][cmd[1][1].index(",") + 1:].strip()]
-                final += draw_pixel(cmd[1][0], cmd[1][1], cmd[1][2])
+                cmd[1] = arguments(cmd[1].strip(), ",", 2)
+                final += draw_pixel(cmd[1][0].strip(), cmd[1][1].strip(), cmd[1][2].strip())
             
             case "pack":
-                cmd[1] = [cmd[1][:cmd[1].index(",")].strip(), cmd[1][cmd[1].index(",") + 1:].strip()]
-                packs[cmd[1][0]] = cmd[1][1]
+                cmd[1] = arguments(cmd[1].strip(), ",", 1)
+                packs[cmd[1][0].strip()] = cmd[1][1].strip()
 
             case "call":
-                final += kx86_compile(packs[cmd[1]], "&")
+                final += kx86_compile(packs[cmd[1].strip()], "&")
 
             case "forever":
                 label = random.randint(1,10000)
                 while label in labels:
                     label = random.randint(1,10000)
                 labels.append(label)
-                final += f"j{label}:\n" + kx86_compile(packs[cmd[1]], "&") + f"\njmp j{label}"
-                
+                final += f"j{label}:\n" + kx86_compile(packs[cmd[1].strip()], "&") + f"\njmp j{label}"
+            
 
     return final
     
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-f", "--file", type=str, nargs="+", help="file or files to compile")
+    parser.add_argument("-o", "--output", type=str, help="file to output to")
 
-parser = argparse.ArgumentParser()
-parser.add_argument("-f", "--file", type=str, nargs="+", help="file or files to compile")
-parser.add_argument("-o", "--output", type=str, help="file to output to")
+    args = parser.parse_args()
+    body = ""
 
-args = parser.parse_args()
-body = ""
+    for i in range(len(args.file)):
+        with open(args.file[i], "r") as f:
+            body += "\n" + f.read()
 
-for i in range(len(args.file)):
-    with open(args.file[i], "r") as f:
-        body += "\n" + f.read()
-
-asm86.kernel += kx86_compile(body)
-asm86.create_image(args.output)
+    asm86.kernel += kx86_compile(body)
+    asm86.create_image(args.output)
