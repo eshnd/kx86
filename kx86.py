2424f4f9fb1d125c33485173975ada68cf346f42
eshnd <eshaan2031@icloud.com>
2025-09-15 19:45:07 -0500

    stuff

--- kx86.py
+++ kx86.py
@@ -2,6 +2,7 @@ import asm86
 import argparse
 
 def draw_pixel(x, y):
+    global asm86
     asm86.kernel += f"""
     mov eax, esi
     mov ebx, {x}
@@ -20,16 +21,14 @@ def kx86_compile(body):
         cmd = body[i].strip()
         cmd.split("(") # can replace with for c in s if need escapes
         cmd.split(")")
-        match cmd[0]:
+        match cmd[0].strip():
             case "draw_pixel":
-                draw_pixel(cmd[1], cmd[2])
+                draw_pixel(cmd[1].strip(), cmd[2].strip())
     
 
 parser = argparse.ArgumentParser()
-group = parser.add_mutually_exclusive_group()
-
-group.add_argument("-f", "--file", type=str, nargs="+", help="file or files to compile")
-group.add_argument("-o", "--output", type=str, help="file to output to")
+parser.add_argument("-f", "--file", type=str, nargs="+", help="file or files to compile")
+parser.add_argument("-o", "--output", type=str, help="file to output to")
 
 args = parser.parse_args()
 body = ""
