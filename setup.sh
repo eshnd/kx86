8ca99840df59d44e5dac6e6ea0f8f1cd14e4f3fc
eshaan <eshaan2031@icloud.com>
2025-09-17 16:33:14 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,2 +1,3 @@
 alias kx86="python3 ~/.kx86/kx86.py"
-printf 'alias kx86="python3 ~/.kx86/kx86.py"'
+printf 'alias kx86="python3 ~/.kx86/kx86.py"' >> ~/.bashrc
+printf 'alias kx86="python3 ~/.kx86/kx86.py"' >> ~/.zshrc
